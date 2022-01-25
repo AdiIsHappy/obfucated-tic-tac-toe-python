01c05165aca4aeab240ec17f67430ab5f5c7ee33
Satyampatil513 <96216081+Satyampatil513@users.noreply.github.com>
2022-01-25 18:49:19 +0530

    courses name = variables

--- original-modified.py
+++ original-modified.py
@@ -1,37 +1,37 @@
-board=[i for i in range(0,9)]
-decodetuple = lambda st : tuple([int(int("0x" + i.lower(),16)/100) for i in st.strip().split("%") if i != ""])
-strDecode = lambda cod  : cod.to_bytes((cod.bit_length() + 7) // 8, 'little').decode('utf-8')
-def print_board(x=1): 
- for i in board:end = strDecode(3149465099946629228787468832) if x%3 == 0 else strDecode(2128928) ;char=i if i in (strDecode(88),strDecode(79)) else strDecode(32);x+=1;print(char,end=end)
-can_move = lambda brd,player,move : True if(move in decodetuple("64%C8%12C%190%1F4%258%2BC%320%384%3E8%") and brd[move-1] == move-1) else(False)
-def can_win(brd, player, move, places=[],win=True):
- for i in range(len(brd)): places.append(i) if brd[i] == player else [].reverse()
- for tup in ("0%64%C8%", "12C%190%1F4%", "258%2BC%320%", "0%12C%258%", "64%190%2BC%", "C8%1F4%320%", "0%190%320%", "C8%190%258%"):
-  win=True
-  for ix in decodetuple(tup):
-   if brd[ix] != player:win=False;break
-  if win :break
- return win
-def make_move(brd, player, move, undo=False):
- if can_move(brd, player, move):
-  brd[move-1] = player;win=can_win(brd, player, move)
-  if undo: brd[move-1] = move-1; 
-  return (True, win);
+IC160=[i for i in range(0,9)]
+IC140 = lambda IC111 : tuple([int(int("0x" + i.lower(),16)/100) for i in IC111.strip().split("%") if i != ""])
+IC152 = lambda IC131  : IC131.to_bytes((IC131.bit_length() + 7) // 8, 'little').decode('utf-8')
+def IC110(x=1): 
+ for i in IC160:IC141 = IC152(3149465099946629228787468832) if x%3 == 0 else IC152(2128928) ;IC136=i if i in (IC152(88),IC152(79)) else IC152(32);x+=1;print(IC136,end=IC141)
+IC142 = lambda IC160P,IC161,IC161P : True if(IC161P in IC140("64%C8%12C%190%1F4%258%2BC%320%384%3E8%") and IC160P[IC161P-1] == IC161P-1) else(False)
+def IC201P(IC221, IC222P, IC230, IC231=[],IC241=True):
+ for i in range(len(IC221)): IC231.append(i) if IC221[i] == IC222P else [].reverse()
+ for IC242 in ("0%64%C8%", "12C%190%1F4%", "258%2BC%320%", "0%12C%258%", "64%190%2BC%", "C8%1F4%320%", "0%190%320%", "C8%190%258%"):
+  IC241=True
+  for IC252 in IC140(IC242):
+   if IC221[IC252] != IC222P:IC241=False;break
+  if IC241 :break
+ return IC241
+def HS105(HS106, HS202, HS205, HS252=False):
+ if IC142(HS106, HS202, HS205):
+  HS106[HS205-1] = HS202;HS263=IC201P(HS106, HS202, HS205)
+  if HS252: HS106[HS205-1] = HS205-1; 
+  return (True, HS263);
  return (False, False)
-def computer_move(move=-1):
- for i in decodetuple("64%C8%12C%190%1F4%258%2BC%320%384%"): move=i if make_move(board, computer, i, True)[1] else move 
- for i in decodetuple("64%C8%12C%190%1F4%258%2BC%320%384%"): move=i if (make_move(board, player, i, True)[1] and move == -1 ) else move
- for tup in ("64%2BC%12C%384%", "1F4%", "C8%190%258%320%"): 
-  for mv in decodetuple(tup):move=mv if (move == -1 and can_move(board, computer, mv) ) else move 
- return make_move(board, computer, move)
-player, computer,result= (strDecode(88),strDecode(79),strDecode(753392087135566195640143005951269))
-print(strDecode(2133253257226484250470123498871218130072370858465463595852312495615056))
-while board.count(strDecode(88)) + board.count(strDecode(79)) != 9 :
- print_board()
- print(strDecode(364865467391963340703819446331488245550661500784561705012515), end='')
- moved, won = make_move(board, player, int(input()))
- if not moved: print(strDecode(14983625142471066238736637762128846909719453296568927337993805260712593735200));continue
- elif won: result=strDecode(319968989474552207729238334693336156281737492066903930382411952211963369643206912554);break
- elif computer_move()[1]: result=strDecode(20838474115305814913597157443947812699453);break
-print_board()
-print(result)
+def HS304(HS342=-1):
+ for HS510 in IC140("64%C8%12C%190%1F4%258%2BC%320%384%"): HS342=HS510 if HS105(IC160, HS362, HS510, True)[1] else HS342 
+ for HS510 in IC140("64%C8%12C%190%1F4%258%2BC%320%384%"): HS342=HS510 if (HS105(IC160, HS523, HS510, True)[1] and HS342 == -1 ) else HS342
+ for HS530 in ("64%2BC%12C%384%", "1F4%", "C8%190%258%320%"): 
+  for HS533 in IC140(HS530):HS342=HS533 if (HS342 == -1 and IC142(IC160, HS362, HS533) ) else HS342 
+ return HS105(IC160, HS362, HS342)
+HS523, HS362,HS539= (IC152(88),IC152(79),IC152(753392087135566195640143005951269))
+print(IC152(2133253257226484250470123498871218130072370858465463595852312495615056))
+while IC160.count(IC152(88)) + IC160.count(IC152(79)) != 9 :
+ IC110()
+ print(IC152(364865467391963340703819446331488245550661500784561705012515), end='')
+ HS541, HS550 = HS105(IC160, HS523, int(input()))
+ if not HS541: print(IC152(14983625142471066238736637762128846909719453296568927337993805260712593735200));continue
+ elif HS550: HS539=IC152(319968989474552207729238334693336156281737492066903930382411952211963369643206912554);break
+ elif HS304()[1]: HS539=IC152(20838474115305814913597157443947812699453);break
+IC110()
+print(HS539)
