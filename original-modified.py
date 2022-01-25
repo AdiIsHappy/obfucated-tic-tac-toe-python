30fa9cca521bcf0fbf9e335c39c1d8305bfbfc8f
adi232004 <96134730+adi232004@users.noreply.github.com>
2022-01-25 14:20:46 +0530

    removed variable names which were used once

--- original-modified.py
+++ original-modified.py
@@ -1,15 +1,11 @@
 board=[i for i in range(0,9)]
-player, computer = '',''
-# Corners, Center and Others, respectively
-moves=("64%2BC%12C%384%", "1F4%", "C8%190%258%320%")
-# Winner combinations
-winners = ("0%64%C8%", "12C%190%1F4%", "258%2BC%320%", "0%12C%258%", "64%190%2BC%", "C8%1F4%320%", "0%190%320%", "C8%190%258%")
-#decoder 
+
 decode = lambda st : tuple([int(int("0x" + i.lower(),16)/100) for i in st.strip().split("%") if i != ""])
-# Table
+
 def print_board():
     x=1
     for i in board:end = ' \n---------\n' if x%3 == 0 else ' | ' ;char=i if i in ('X','O') else ' ';x+=1;print(char,end=end)
+
 can_move = lambda brd,player,move : True if(move in decode("64%C8%12C%190%1F4%258%2BC%320%384%3E8%") and brd[move-1] == move-1) else(False)
 def can_win(brd, player, move):
     places=[]
@@ -18,7 +14,7 @@ def can_win(brd, player, move):
         if i == player: places.append(x);
         x+=1
     win=True
-    for tup in winners:
+    for tup in("0%64%C8%", "12C%190%1F4%", "258%2BC%320%", "0%12C%258%", "64%190%2BC%", "C8%1F4%320%", "0%190%320%", "C8%190%258%"):
         win=True
         for ix in decode(tup):
             if brd[ix] != player:
@@ -27,27 +23,23 @@ def can_win(brd, player, move):
         if win == True:
             break
     return win
+
 def make_move(brd, player, move, undo=False):
     if can_move(brd, player, move):
-        brd[move-1] = player
-        win=can_win(brd, player, move)
-        if undo:
-            brd[move-1] = move-1
-        return (True, win)
+        brd[move-1] = player;win=can_win(brd, player, move)
+        if undo: brd[move-1] = move-1; 
+        return (True, win);
     return (False, False)
-# AI goes here
 def computer_move():
     move=-1
     for i in decode("64%C8%12C%190%1F4%258%2BC%320%384%"): move=i if make_move(board, computer, i, True)[1] else move 
     for i in decode("64%C8%12C%190%1F4%258%2BC%320%384%"): move=i if (make_move(board, player, i, True)[1] and move == -1 ) else move
-    for tup in moves: 
+    for tup in ("64%2BC%12C%384%", "1F4%", "C8%190%258%320%"): 
         for mv in decode(tup):move=mv if (move == -1 and can_move(board, computer, mv) ) else move 
     return make_move(board, computer, move)
 
-
-player, computer = ("X","O")
+player, computer,result= ("X","O",'%%% Draw ! %%%')
 print(f"Player is {player} and computer is {computer}" )
-result='%%% Draw ! %%%'
 while board.count('X') + board.count('O') != 9 :
     print_board()
     print('#Make your move ! [1-9] : ', end='')
