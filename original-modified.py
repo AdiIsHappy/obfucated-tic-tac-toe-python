cee30ea34b4742ae28b5ba7fa530a1fb94737828
adi232004 <96134730+adi232004@users.noreply.github.com>
2022-01-25 23:34:31 +0530

    final code :)

--- original-modified.py
+++ original-modified.py
@@ -1,37 +1,40 @@
-IC160=[i for i in range(0,9)];
-IC140=lambda IC111:tuple([int(int("0x"+i.lower(),16)/100)for i in IC111.strip().split("%")if i!=""])
-IC152=lambda IC131:IC131.to_bytes((IC131.bit_length()+7)//8,'little').decode('utf-8')
-def IC110(x=1): 
- for i in IC160:IC141=IC152(3149465099946629228787468832)if x%3==0 else IC152(2128928);IC136=i if i in(IC152(88),IC152(79))else IC152(32);x+=1;print(IC136,end=IC141)
-IC142=lambda IC160P,IC161,IC161P:True if(IC161P in IC140("64%C8%12C%190%1F4%258%2BC%320%384%3E8%")and IC160P[IC161P-1]==IC161P-1)else(False)
-def IC201P(IC221,IC222P,IC230,IC231=[],IC241=True):
- for i in range(len(IC221)):IC231.append(i) if IC221[i]==IC222P else [].reverse()
- for IC242 in ("0%64%C8%","12C%190%1F4%","258%2BC%320%","0%12C%258%","64%190%2BC%","C8%1F4%320%","0%190%320%","C8%190%258%"):
-  IC241=True
-  for IC252 in IC140(IC242):
-   if IC221[IC252]!=IC222P:IC241=False;break
-  if IC241:break
- return IC241
-def HS105(HS106,HS202,HS205,HS252=False):
- if IC142(HS106,HS202,HS205):
-  HS106[HS205-1]=HS202;HS263=IC201P(HS106,HS202,HS205)
-  if HS252:HS106[HS205-1]=HS205-1; 
-  return(True,HS263);
- return(False,False)
-def HS304(HS342=-1):
- for HS510 in IC140("64%C8%12C%190%1F4%258%2BC%320%384%"):HS342=HS510 if HS105(IC160,HS362,HS510,True)[1]else HS342 
- for HS510 in IC140("64%C8%12C%190%1F4%258%2BC%320%384%"):HS342=HS510 if (HS105(IC160,HS523,HS510,True)[1]and HS342==-1)else HS342
- for HS530 in ("64%2BC%12C%384%","1F4%","C8%190%258%320%"): 
-  for HS533 in IC140(HS530):HS342=HS533 if (HS342==-1 and IC142(IC160,HS362,HS533))else HS342 
- return HS105(IC160,HS362,HS342)
-HS523,HS362,HS539=(IC152(88),IC152(79),IC152(753392087135566195640143005951269))
-print(IC152(2133253257226484250470123498871218130072370858465463595852312495615056))
-while IC160.count(IC152(88))+IC160.count(IC152(79))!=9:
- IC110()
- print(IC152(364865467391963340703819446331488245550661500784561705012515),end='')
- HS541,HS550=HS105(IC160,HS523,int(input()))
- if not HS541:print(IC152(14983625142471066238736637762128846909719453296568927337993805260712593735200));continue
- elif HS550:HS539=IC152(319968989474552207729238334693336156281737492066903930382411952211963369643206912554);break
- elif HS304()[1]:HS539=IC152(20838474115305814913597157443947812699453);break
-IC110()
-print(HS539)
+IC160=[i for i in range(0,9)];([[j for j in range(i)   ]for i in range(10)]*100 ).sort( ) ;[ j for j in range(1)   ].sort;["4%C10%258%2BC%320%384%3E8%325%7DF4567%S"]
+IC140=lambda IC111:tuple([int(int("0x"+i.lower(),16)   /100)for i in IC111.strip().split("%")if i!=""]);str(int(   34+4.574)+int(23.4)+int(436.346))+"35%322%46%4662"
+IC152=lambda IC131:IC131.to_bytes((IC131.bit_length(   )+7)//8,'little').decode('utf-8');print( "hii") if 0 == 1   else [2,].sort;pqr  = 3149465099946629228787468832
+def IC110(x=1):#in IC111.strip().split("%")if i!=""]   );str(int(3496034.574)+int(23.4)+int(436.346)) + "35%3226   62"+IC140("64%C8%12C%190%1F4%258%2BC%320%384%"):HS
+ for i in IC160:IC141=IC152(pqr ) if x % 3 == 0 else   IC152(2128928);IC136=i if i in(IC152(88),IC152(79) ) else   IC152(32);x+=1;print(IC136,end=IC141);[].append(4)
+IC142=lambda IC160P,IC161,IC161P: True if (IC161P in   IC140("64%C8%12C%190%1F4%258%2BC%320%384%3E8%")and IC160P   [IC161P-1]==IC161P-1)else(False);mk="258%2BC%320%"
+def IC201P(IC221,IC222P,IC230,IC231=[],IC241=True):#   print(IC152(213325325722648425047012349887121813007237085   5463595852312495615056))[[j for j in [i for i iran
+ for i in range(len(IC221)):IC231.append(i) if IC221   [i]==IC222P else [].reverse();print("hello my love, i lov   e you") if 23 == 64 else [].append("35%32266%352")
+ for IC242 in("0%64"+"%C8%","12C%190%1F4%","258%2BC"   +"%320%" , "0%12C%258%","64%190%2BC%","C8%1F4%320%","0%1"   +"90%320%","C8%190%258%"): #[j for j in range(1)].
+  IC241=True;[[j for j in [i for i in  range(10) ] ]   for k  in range(2000)].append([[j for j in range(i)]for i   in range(10)]);[IK345 for IK345 in ["12C%1%258%"]]
+  for IC252 in IC140(IC242):#61P:True if(IC161P in I   C140("64%C8%12C%190%1F4%258%2BC%320%384%3E8%")and IC160P[   IC161P-1]==IC161P-1)else(Fal for i in range(4050)]
+   if IC221[IC252]!=IC222P:IC241=False;break; FI325=   lambda x : 0 if x == 0 else(1 if x==1 else fib(x-1) + fib   (x-2));[IK345 for IK345 in ["12C%1F4%258%345%32"]]
+
+
+  if IC241:break;[j for j in range(1)].sort;["4%C8%1   2C%190%1F4%258%2BC%320%384%3E8%325%DF4567%S"];AB357= "hel   sweet heart how are you" if 45>3 else "you ugly y"
+ return IC241;pi = 3.1498;([0,pi/4,pi/2,3*pi/4,pi,5*   pi/4,3*pi/2,7*pi/4,2*pi],["0","$\pi$/4","$\pi$/2","3$\pi$   /4","$\pi$","i$/4","3$i$/2","7$\pi$/4", "2$\pi$"])
+def HS105(HS106,HS202,HS205,HS252=False):#HS510 in I   C140("64%C8%12C%190%1F4%258%2BC%320%384%"):HS342=HS510 if   (HS105(IC160,HS523,HS510,True)[ HS342==-1)else HS3
+ if IC142(HS106,HS202,HS205):#bytes((IC131.bit_lengt   h()+7)//8,'little').decode('utf-8');BP456 = str(int(34662   63.4362)) +4%320%" + "C8%190%2320%" if True else "
+  HS106[HS205-1]= HS202; HS263= IC201P (HS106,HS202,   HS205);[[float(i) for i in "235 362".strip().split()] for   i in range(int("325"))];"Caso {t}: {leeros+fgkkjh"
+  if HS252:HS106[HS205-1]=HS205-1;get_strings_list =   [i for i in "4%C8%12C%190%1F4%258%2BC%320%384%3E8%325%DF4   567%S".strip().split()];["why wrry brm here fr u"]
+  return(True,HS263);[AB244 + AB244 +AB244 for AB244   in range (10)].append([AB244*2 for AB244 in range(10)]);[   "4%C8%12C%190%1F4%258320%384%3E8%325%DF4567%S24%"]
+ return(False,False);([[j for j in range(i)]for i in   range(10)]*100).sort();[j for j in range(1)].sort;["4%C8%   12C%190%58%2BC%32%384%3E8%325%567%S"].append("35")
+def HS304(HS342=-1):#in range(len(IC221)):IC231.appe   nd(i) if IC221[i]==IC222P else [].reverse();print("hel lo   my love, i love you") == 64 else [].append("35%3")
+ for HS510 in IC140("64%C8%12C%190%1F4%258%2BC%320%"   +"384%"):HS342=HS510 if HS105(IC160, HS362, HS510, True)[   1]else HS342;AB357= "hew are you" if 45>3 else "y"
+ for HS510 in IC140("64%C8%12C%190%1F4%258%2BC%320%"   +"384%"):HS342=HS510 if (HS105(IC160,HS523,HS510,True)[1]   and HS342==-1) else HS342; [A for A in range (23)]
+ for HS530 in ("64%2BC%12C%384%","1F4%","C8%190%258"   +"%320%"):#e (10)].append([AB244*2 for AB244 in range(10)   ]);["4%C8%12C%190%1F4%258%2BC%320%38%DF4567%S24%"]
+
+
+  for HS533 in IC140(HS530):HS342=HS533 if (HS342==-   1 and IC142(IC160, HS362, HS533)) else HS342;[34 for i in   range(1,100,5)].append( "4%C8%12C%258%2BC%32%384")
+ return HS105(IC160,HS362,HS342);print("hi my family   is ready for marrieg. will you marry me") if 23 ==64 else   [].append("35%322%436 %352");["plz say yes i l u"]
+HS523, HS362, HS539 = ( IC152 (88), IC152(79),IC152(   753392087135566195640143005951269));e = 2.7; [e, e/2, e/3,  e/4, e/5, e/6, e/7 , e/8].append("this isto run ")
+print(IC152(int("2133253257226484250470123498871218"   +"130072370858465463595852312495615056"))); [ chr(I)for I   in range(50,120)].append([i+2 for i in range(12)])
+while IC160.count(IC152(88))+ IC160.count(IC152(79))   !=9:#in range (10)].append([AB244*2 for AB244 in range(10   )]);["4%C8%12C%190%1F58%2BC%320%3325%DF4567%S24%"]
+ IC110();[[j for j in [i for i in  range(10)]] for k   in range (2000) ].append( [ [j for j in range(i)]for i in   range(10)]);[IK345 for IK345 in ["12C%%258%2%43"]]
+ print(IC152(int("364865467391963340703819446331488"   +"245550661500784561705012515")),end=''); ( [ [j for j in   range(i)]for i in range(10)]).sort();[2423, 57, 3]
+ HS541,HS550=HS105(IC160,HS523,int(input())); IC140(   "64%C8%12C%190%1F4%258%2BC%320%384%");["4%C8%12C%190%1F4%   258%2BC%%3E8%325%DF4567%S"];[i for i in range(19)]
+ if not HS541:print(IC152(int("14983625142471066238"   +"736637762128846909719453296568927337993805260712593735"   +"200")));continue;IC161P in IC140( "64%C8%%320%")
+ elif HS550:HS539=IC152(int("3199689894745522077292"   +"383346933361562817374920669039303824119522119633696432"   +"06912554"));break;#]);[IK34C%190%1F4%258%2%43"]]
+ elif HS304()[1]:HS539=IC152(int("20838474115305814"   +"913597157443947812699453"));break;[[j for j in range(i)   ]for i in range(10)]*100;"X362" in "64%C8%12C%232"
+IC110();print(HS539);["THANKYOU"]#44 +AB244 for AB24   4 in range (10)].append([AB244*2 for AB244 in range(10)])   ;["4%C8%12C%190%1F4%258%2BC%320%384%3%DF4567%S2%"]
