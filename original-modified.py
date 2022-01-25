5af508e806614b962fdfde049d9cb83b9ade68bd
Satyampatil513 <96216081+Satyampatil513@users.noreply.github.com>
2022-01-25 18:59:40 +0530

    changed I's

--- original-modified.py
+++ original-modified.py
@@ -1,11 +1,11 @@
-IC160=[i for i in range(0,9)]
+IC160=[CS302 for CS302 in range(0,9)]
 IC140 = lambda IC111 : tuple([int(int("0x" + i.lower(),16)/100) for i in IC111.strip().split("%") if i != ""])
 IC152 = lambda IC131  : IC131.to_bytes((IC131.bit_length() + 7) // 8, 'little').decode('utf-8')
-def IC110(x=1): 
- for i in IC160:IC141 = IC152(3149465099946629228787468832) if x%3 == 0 else IC152(2128928) ;IC136=i if i in (IC152(88),IC152(79)) else IC152(32);x+=1;print(IC136,end=IC141)
+def IC110(CS307=1): 
+ for HS632 in IC160:IC141 = IC152(3149465099946629228787468832) if CS307%3 == 0 else IC152(2128928) ;IC136=HS632 if HS632 in (IC152(88),IC152(79)) else IC152(32);CS307+=1;print(IC136,end=IC141)
 IC142 = lambda IC160P,IC161,IC161P : True if(IC161P in IC140("64%C8%12C%190%1F4%258%2BC%320%384%3E8%") and IC160P[IC161P-1] == IC161P-1) else(False)
 def IC201P(IC221, IC222P, IC230, IC231=[],IC241=True):
- for i in range(len(IC221)): IC231.append(i) if IC221[i] == IC222P else [].reverse()
+ for CS201 in range(len(IC221)): IC231.append(CS201) if IC221[CS201] == IC222P else [].reverse()
  for IC242 in ("0%64%C8%", "12C%190%1F4%", "258%2BC%320%", "0%12C%258%", "64%190%2BC%", "C8%1F4%320%", "0%190%320%", "C8%190%258%"):
   IC241=True
   for IC252 in IC140(IC242):
