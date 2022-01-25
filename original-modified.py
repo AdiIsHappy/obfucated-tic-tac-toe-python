f12648c73e0236e19d0681d054175607dd256312
adi232004 <96134730+adi232004@users.noreply.github.com>
2022-01-25 15:40:06 +0530

    shorted  Can_win

--- original-modified.py
+++ original-modified.py
@@ -1,27 +1,17 @@
 board=[i for i in range(0,9)]
-
 decodetuple = lambda st : tuple([int(int("0x" + i.lower(),16)/100) for i in st.strip().split("%") if i != ""])
 strDecode = lambda cod  : cod.to_bytes((cod.bit_length() + 7) // 8, 'little').decode('utf-8')
-
 def print_board(x=1): 
-    for i in board:end = strDecode(3149465099946629228787468832) if x%3 == 0 else strDecode(2128928) ;char=i if i in (strDecode(88),strDecode(79)) else strDecode(32) ;x+=1;print(char,end=end)
-
+    for i in board:end = strDecode(3149465099946629228787468832) if x%3 == 0 else strDecode(2128928) ;char=i if i in (strDecode(88),strDecode(79)) else strDecode(32);x+=1;print(char,end=end)
 can_move = lambda brd,player,move : True if(move in decodetuple("64%C8%12C%190%1F4%258%2BC%320%384%3E8%") and brd[move-1] == move-1) else(False)
-def can_win(brd, player, move, places=[],x=0 ):
-    for i in brd:
-        if i == player: places.append(x);
-        x+=1
-    win=True
+def can_win(brd, player, move, places=[],win=True):
+    for i in range(len(brd)): places.append(i) if brd[i] == player else [].reverse()
     for tup in [2681967405167945008,11514140978233693408361394737,11509210864524423570633798962,175764410701507988628784,45047874978549708754334774,44957854939532054061463619,175616620857529688204592,44995689139565184432814147]:
         win=True
         for ix in decodetuple(strDecode( tup)):
-            if brd[ix] != player:
-                win=False
-                break
-        if win == True:
-            break
+            if brd[ix] != player:win=False;break
+        if win :break
     return win
-
 def make_move(brd, player, move, undo=False):
     if can_move(brd, player, move):
         brd[move-1] = player;win=can_win(brd, player, move)
@@ -34,7 +24,6 @@ def computer_move(move=-1):
     for tup in ("64%2BC%12C%384%", "1F4%", "C8%190%258%320%"): 
         for mv in decodetuple(tup):move=mv if (move == -1 and can_move(board, computer, mv) ) else move 
     return make_move(board, computer, move)
-
 player, computer,result= ("X","O",'%%% Draw ! %%%')
 print(f"Player is {player} and computer is {computer}" )
 while board.count('X') + board.count('O') != 9 :
